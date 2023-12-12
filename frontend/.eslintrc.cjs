adb680a57842cda2a74901e55081264f34246ff1
Harri Nieminen <harri.nieminen@posteo.fi>
2023-12-12 13:30:43 +0200

    frontend: fix eslint

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -11,6 +11,12 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
@@ -18,4 +24,4 @@ module.exports = {
       { allowConstantExport: true },
     ],
   },
-}
+};
