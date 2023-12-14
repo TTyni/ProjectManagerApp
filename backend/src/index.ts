2ab20048efcc0f07e9820d622c0916f46c2937be
Jambo258 <jambomyllymaa@gmail.com>
2023-12-14 10:49:00 +0200

    backend: project routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import usersRouter from "./routes/userRouter.js";
+import projectsRouter from "./routes/projectRouter.js";
 dotenv.config();
 
 const server = express();
@@ -11,5 +12,6 @@ server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
 
 server.use("/user", usersRouter);
+server.use("/projects", projectsRouter);
 
 server.listen(PORT, () => console.log("Listening to port", PORT));
