82092b0ab2443e2931e0549616306e401b41e0d5
Harri Nieminen <harri.nieminen@posteo.fi>
2023-12-19 15:17:51 +0200

    backend: imporve register error messages

--- backend/src/middlewares/validate.ts
+++ backend/src/middlewares/validate.ts
@@ -3,11 +3,18 @@ import { type AnySchema, ValidationError } from "yup";
 
 const validate = (schema: AnySchema): RequestHandler => async (req, res, next) => {
   try {
-    await schema.validate(req.body);
+    await schema.validate(req.body, { abortEarly: false });
     next();
   } catch (err) {
     if (err instanceof ValidationError) {
-      return res.status(400).json({ error: err.message });
+      const errorMessages = err.inner.reduce((errors, inner) => {
+        if (inner.path) {
+          return { ...errors, [inner.path]: inner.errors.join()};
+        } else {
+          return errors;
+        }
+      }, {error: err.message});
+      return res.status(400).json(errorMessages);
     } else {
       next(err);
     }
