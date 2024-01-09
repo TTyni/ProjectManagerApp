6ff0755bed93e202be60b598dc1e91b8feeeb235
Harri Nieminen <harri.nieminen@posteo.fi>
2024-01-09 15:26:00 +0200

    frontend: add loader spinner

--- frontend/src/pages/views/homeView.tsx
+++ frontend/src/pages/views/homeView.tsx
@@ -1,3 +1,4 @@
+import { Loader } from "react-feather";
 import { useGetProjectsQuery } from "../../features/api/apiSlice";
 
 export const HomeView = () => {
@@ -6,7 +7,7 @@ export const HomeView = () => {
   let projectList;
 
   if (isLoading) {
-    projectList = <p className="mb-4">Loading...</p>;
+    projectList = <p className="mb-4"><Loader className="animate-spin" /></p>;
   } else if (isSuccess) {
     projectList = <ul className="list-decimal mb-4">{projects.map(p => <li key={p.id}>{p.name}</li>)}</ul>;
   } else if (isError) {
@@ -17,19 +18,17 @@ export const HomeView = () => {
     <main className="grid grid-flow-col max-w-7xl m-auto mt-12">
       <div className="w-96 h-96 bg-grayscale-300 hidden mx-6 lg:flex" />
       <div className="body-text-lg mx-8">
-
         <p className="heading-xl mb-8">
-            Lorem ipsum, dolor sit amet consectetur!
+          Lorem ipsum, dolor sit amet consectetur!
         </p>
         <p className="mb-4">
-            Introducing our cutting-edge <span className="font-semibold">Project Management App</span> - your ultimate solution for streamlined collaboration, efficient task tracking,
-            and seamless communication. Empower your team to achieve project milestones with ease, all within a user-friendly interface designed to enhance productivity and project success.</p>
+          Introducing our cutting-edge <span className="font-semibold">Project Management App</span> - your ultimate solution for streamlined collaboration, efficient task tracking,
+          and seamless communication. Empower your team to achieve project milestones with ease, all within a user-friendly interface designed to enhance productivity and project success.</p>
         <p className="mb-4">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat nobis dignissimos repudiandae, at maxime impedit omnis corporis dolores vero modi quisquam aut a explicabo.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat nobis dignissimos repudiandae, at maxime impedit omnis corporis dolores vero modi quisquam aut a explicabo.
         </p>
         {projectList}
       </div>
     </main>
-      
   );
 };
