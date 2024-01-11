049b6988845fbafe8642182f548098cd80219c8a
Katariina Ruotsalainen <bkruotsalainen@gmail.com>
2024-01-11 13:55:23 +0200

    Remove example content from project view

--- frontend/src/pages/views/projectView.tsx
+++ frontend/src/pages/views/projectView.tsx
@@ -1,36 +1,6 @@
 export const ProjectView = () => {
   return (
     <section className="p-12 max-h-full overflow-auto">
-      
-      <div className="flex flex-row flex-nowrap gap-6 overflow-x-scroll p-4 border border-grayscale-300 rounded-md mb-6">      
-        <div className="flex-shrink-0 max-w-lg p-4 rounded-md bg-grayscale-100">
-          <p className="heading-sm">Example</p>
-          <p className="body-text-md">This is example of very very wide object. Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, numquam.</p>
-        </div>
-        <div className="flex-shrink-0 max-w-lg p-4 rounded-md bg-grayscale-100">
-          <p className="heading-sm">Example</p>
-          <p className="body-text-md">This is example of very very wide object. Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, numquam.</p>
-        </div>
-        <div className="flex-shrink-0 max-w-lg p-4 border border-grayscale-300 rounded-md">
-        </div>
-        <div className="flex-shrink-0 max-w-lg p-4 rounded-md bg-grayscale-100">
-          <p className="heading-sm">Example</p>
-          <p className="body-text-md">This is example of very very wide object. Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, numquam.</p>
-        </div>
-        <div className="flex-shrink-0 max-w-lg p-4 rounded-md bg-grayscale-100">
-          <p className="heading-sm">Example</p>
-          <p className="body-text-md">This is example of very very wide object. Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, numquam.</p>
-        </div>
-        <div className="flex-shrink-0 max-w-lg p-4 rounded-md bg-grayscale-100">
-          <p className="heading-sm">Example</p>
-          <p className="body-text-md">This is example of very very wide object. Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, numquam.</p>
-        </div>
-        <div className="flex-shrink-0 max-w-lg p-4 rounded-md bg-grayscale-100">
-          <p className="heading-sm">Example</p>
-          <p className="body-text-md">This is example of very very wide object. Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, numquam.</p>
-        </div>
-      </div>
-
     </section>
   );
 };
