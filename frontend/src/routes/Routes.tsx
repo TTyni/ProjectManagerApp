a98947b03a8bec8f8e2b8c4d6f4c0437b93cc20f
Harri Nieminen <moiman@posteo.net>
2024-01-17 15:22:14 +0200

    frontend: remove empty App component (#77)

--- frontend/src/routes/Routes.tsx
+++ frontend/src/routes/Routes.tsx
@@ -5,7 +5,6 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
-import App from "../app/App.tsx";
 import { PublicPage } from "../features/frontpage/PublicPage.tsx";
 import { RegisterView } from "../features/auth/RegisterView.tsx";
 import { LoginView } from "../features/auth/LoginView.tsx";
@@ -21,20 +20,18 @@ export const AppRouter = () => {
   console.log(user);
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path="/" element={<App />}>
-        <Route path="/" element={user ? <PrivatePage /> : <PublicPage />}>
-          <Route index element={user ? <ProjectView /> : <HomeView />} />
-          <Route path="login" element={<LoginView />} />
-          {user && (
-            <>
-              <Route path="projects/:projectId/:pageId" element={<Editor />} />
-              <Route path="projects/:projectId" element={<ProjectView />} />
-              <Route path="profile" element={<ProfileModal />} />
-            </>
-          )}
-          <Route path="register" element={<RegisterView />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Route>
+      <Route path="/" element={user ? <PrivatePage /> : <PublicPage />}>
+        <Route index element={user ? <ProjectView /> : <HomeView />} />
+        <Route path="login" element={<LoginView />} />
+        {user && (
+          <>
+            <Route path="projects/:projectId/:pageId" element={<Editor />} />
+            <Route path="projects/:projectId" element={<ProjectView />} />
+            <Route path="profile" element={<ProfileModal />} />
+          </>
+        )}
+        <Route path="register" element={<RegisterView />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Route>
     )
   );
