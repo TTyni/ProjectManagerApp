5380c044dd22a5f50d067c64d36540e4d9b06ab9
susulone <81521009+susulone@users.noreply.github.com>
2024-01-19 14:56:04 +0200

    frontend: changes to the ui of profile modal (#85)
    * ProfileModal: make changes to the forms' ui
    
    * fix code formatting

--- frontend/src/features/user/ProfileModal.tsx
+++ frontend/src/features/user/ProfileModal.tsx
@@ -1,11 +1,21 @@
+// React
 import { useState } from "react";
-import { DeleteModal } from "../../components/DeleteModal";
+
+// Redux Toolkit
 import { useDeleteUserMutation, useUpdateUserMutation } from "../api/apiSlice";
-import { changeEmailSchema, changeNameSchema, changePasswordSchema } from "../auth/authValidation";
+
+// React Router
+import { useNavigate } from "react-router-dom";
+
+// Hook Form and Yup
 import { FieldErrors, useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
+
+// Components
 import { useAppSelector } from "../../app/hooks";
-import { useNavigate } from "react-router-dom";
+import { changeEmailSchema, changeNameSchema, changePasswordSchema } from "../auth/authValidation";
+import { DeleteModal } from "../../components/DeleteModal";
+import { Eye, EyeOff } from "react-feather";
 
 interface changeNameFormValues {
   name: string;
@@ -21,69 +31,42 @@ interface changePasswordFormValues {
 
 export const ProfileModal = () => {
   const user = useAppSelector((state) => state.auth.user);
+  const [updateUser, { isLoading }] = useUpdateUserMutation();
+  const [deleteUser]  = useDeleteUserMutation();
+  const navigate = useNavigate();
+  const canSave = !isLoading;
   console.log(user);
 
+  // Change Name
   const [nameEdit, setNameEdit] = useState(false);
-  const [emailEdit, setEmailEdit] = useState(false);
-  const [passwordEdit, setPasswordEdit] = useState(false);
-  const [confirmDeleteEdit, setConfirmDeleteEdit] = useState(false);
   const [formErrorName, setNameFormError] = useState<null | string>(null);
-  const [formErrorEmail, setEmailFormError] = useState<null | string>(null);
-  const [formErrorPassword, setPasswordFormError] = useState<null | string>(null);
-  const navigate = useNavigate();
-  const deleteModalText = "Are you sure you want to delete this account?";
-
-  const [updateUser, { isLoading }] = useUpdateUserMutation();
-
-  const [deleteUser]  = useDeleteUserMutation();
-
-  const canSave = !isLoading;
-
   const {
     register: registerName,
     reset: resetName,
     handleSubmit: handleName,
-    formState: {errors: errorsName },
+    formState: { errors: errorsName },
   } = useForm<changeNameFormValues>({
     defaultValues: {
-      name: "",
+      name: user?.name,
     },
     resolver: yupResolver(changeNameSchema),
   });
 
-  const {
-    register: registerEmail,
-    reset: resetEmail,
-    handleSubmit: handleEmail,
-    formState: { errors: errorsEmail },
-  } = useForm<changeEmailFormValues>({
-    defaultValues: {
-      email: "",
-    },
-    resolver: yupResolver(changeEmailSchema),
-  });
-
-  const {
-    register: registerPassword,
-    reset: resetPassword,
-    formState: { errors: errorsPassword },
-    handleSubmit: handlePassword,
-  } = useForm<changePasswordFormValues>({
-    defaultValues: {
-      password: "",
-    },
-    resolver: yupResolver(changePasswordSchema),
-  });
-
   const onHandleSubmitName = async (formData: changeNameFormValues) => {
-    if (canSave) {
+    if (user?.name === formData.name) {
+      setNameEdit(false);
+    } else if (canSave) {
       try {
         const user = await updateUser({
           name: formData.name,
         }).unwrap();
+        // For development only
         console.log("User:", user);
-        resetName();
-        setNameFormError(null);
+        if (user) {
+          resetName({ name: user.name });
+          setNameFormError(null);
+          setNameEdit(false);
+        }
       } catch (err) {
         onErrorName;
         console.error("Failed to save the user", err);
@@ -101,15 +84,36 @@ export const ProfileModal = () => {
     }
   };
 
+  // Change Email
+  const [emailEdit, setEmailEdit] = useState(false);
+  const [formErrorEmail, setEmailFormError] = useState<null | string>(null);
+  const {
+    register: registerEmail,
+    reset: resetEmail,
+    handleSubmit: handleEmail,
+    formState: { errors: errorsEmail },
+  } = useForm<changeEmailFormValues>({
+    defaultValues: {
+      email: user?.email,
+    },
+    resolver: yupResolver(changeEmailSchema),
+  });
+
   const onHandleSubmitEmail = async (formData: changeEmailFormValues) => {
-    if (canSave) {
+    if (user?.email === formData.email) {
+      setEmailEdit(false);
+    } else if (canSave) {
       try {
         const user = await updateUser({
           email: formData.email,
         }).unwrap();
+        // For development only
         console.log("User:", user);
-        resetEmail();
-        setEmailFormError(null);
+        if (user) {
+          resetEmail({ email: user.email });
+          setEmailFormError(null);
+          setEmailEdit(false);
+        }
       } catch (err) {
         onErrorEmail;
         console.error("Failed to save the user", err);
@@ -127,15 +131,35 @@ export const ProfileModal = () => {
     }
   };
 
+  // Change Password
+  const [passwordEdit, setPasswordEdit] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const [formErrorPassword, setPasswordFormError] = useState<null | string>(null);
+  const {
+    register: registerPassword,
+    reset: resetPassword,
+    formState: { errors: errorsPassword },
+    handleSubmit: handlePassword,
+  } = useForm<changePasswordFormValues>({
+    defaultValues: {
+      password: "",
+    },
+    resolver: yupResolver(changePasswordSchema),
+  });
+
   const onHandleSubmitPassword = async (formData: changePasswordFormValues) => {
     if (canSave) {
       try {
         const user = await updateUser({
           password: formData.password,
         }).unwrap();
+        // For development only
         console.log("User:", user);
-        resetPassword();
-        setPasswordFormError(null);
+        if (user) {
+          resetPassword();
+          setPasswordFormError(null);
+          setPasswordEdit(false);
+        }
       } catch (err) {
         onErrorPassword;
         console.error("Failed to save the user", err);
@@ -153,6 +177,22 @@ export const ProfileModal = () => {
     }
   };
 
+  // Delete User
+  const [confirmDeleteEdit, setConfirmDeleteEdit] = useState(false);
+  const deleteModalText = "Are you sure you want to delete this account?";
+
+  const handleSubmitForModal = async () => {
+    try {
+      const user = await deleteUser().unwrap();
+      // For development only
+      console.log("User deleted:", user);
+      navigate("/");
+
+    } catch (err) {
+      console.error("Failed to delete user", err);
+    }
+  };
+
   const onErrorName = (errors: FieldErrors<changeNameFormValues>) => {
     console.log("Form field errors:", errors);
   };
@@ -161,17 +201,6 @@ export const ProfileModal = () => {
   };
   const onErrorPassword = (errors: FieldErrors<changePasswordFormValues>) => {
     console.log("Form field errors:", errors);
-  };
-
-  const handleSubmitForModal = async () => {
-    try {
-      const user = await deleteUser().unwrap();
-      console.log("User deleted:", user);
-      navigate("/");
-
-    } catch (err) {
-      console.error("Failed to delete user", err);
-    }
   };
 
   const resetFields = () => {
@@ -184,161 +213,195 @@ export const ProfileModal = () => {
   };
 
   return (
-    <>
-      <div className="max-w-2xl m-auto grid grid-cols-4 gap-x-3 grid-flow-row items-center body-text-sm">
-        <section className="col-span-3 my-4">
-          <p className="heading-xs">Name</p>
-          <p>{user?.name}</p>
-        </section>
-        <button
-          type="button"
-          onClick={() => {
-            setNameEdit(!nameEdit);
-            setEmailEdit(false);
-            setPasswordEdit(false);
-            resetFields();
-          }}
-          className="col-span-1 mt-4 py-2 btn-text-xs"
-        >
-          Change Name
-        </button>
-        {nameEdit && (
-          <section className="col-span-3">
-            <form onSubmit={handleName(onHandleSubmitName, onErrorName)}>
-              <label>
-                Name
-                <input
-                  type="text"
-                  {...registerName("name")}
-                  className="block w-full py-2 px-4 mb-3 body-text-md focus:outline-none focus:ring focus:ring-dark-blue-50"
-                />
-                <p className="mt-1 text-center body-text-xs text-caution-200">
-                  {errorsName.name?.message}
-                </p>
-                <p className="mt-1 text-center body-text-xs text-caution-200">
-                  {formErrorName}
-                </p>
-              </label>
-              <button
-                type="submit"
-                className="col-span-1 mb-4 py-2 btn-text-xs"
-              >
-                Submit
-              </button>
-            </form>
-          </section>
-        )}
+    <div className="min-w-full max-w-2xl m-auto grid grid-cols-4 gap-x-3 grid-flow-row items-center body-text-sm">
 
-        <section className="col-span-3 my-4">
-          <p className="heading-xs">Email</p>
-          <p>{user?.email}</p>
-        </section>
-        <button
-          type="button"
-          onClick={() => {
-            setNameEdit(false);
-            setEmailEdit(!emailEdit);
-            setPasswordEdit(false);
-            resetFields();
-          }}
-          className="my-4 py-2 col-span-1 btn-text-xs"
-        >
-          Change Email
-        </button>
-        {emailEdit && (
+      {nameEdit ? (
+        <form
+          onSubmit={handleName(onHandleSubmitName, onErrorName)}
+          className="col-span-4 grid grid-cols-4 gap-x-3 grid-row-1">
           <section className="col-span-3">
-            <form onSubmit={handleEmail(onHandleSubmitEmail, onErrorEmail)}>
-              <label>
-                Email
-                <input
-                  type="email"
-                  {...registerEmail("email")}
-                  className="body-text-md py-1.5 px-4 mb-3 w-full block focus:outline-none focus:ring focus:ring-dark-blue-50"
-                />
-                <p className="text-center body-text-xs text-caution-200 mt-1">
-                  {errorsEmail.email?.message}
-                </p>
-                <p className="text-center body-text-xs text-caution-200 mt-1">
-                  {formErrorEmail}
-                </p>
-              </label>
-              <button
-                type="submit"
-                className="col-span-1 my-4 py-2 btn-text-xs"
-              >
-                Submit
-              </button>
-            </form>
+            <label className="heading-xs">
+              Name
+              <input
+                type="text"
+                {...registerName("name")}
+                className="block w-full py-1.5 px-4 mb-3 body-text-md focus:outline-none focus:ring focus:ring-dark-blue-50"
+              />
+              <p className="mt-1 text-center body-text-xs text-caution-200">
+                {errorsName.name?.message}
+              </p>
+              <p className="mt-1 text-center body-text-xs text-caution-200">
+                {formErrorName}
+              </p>
+            </label>
           </section>
-        )}
-        <section className="col-span-3 my-4">
-          <p className="heading-xs">Password</p>
-          <p>Set a permanent password to login to your account.</p>
-        </section>
-        <button
-          type="button"
-          onClick={() => {
-            setNameEdit(false);
-            setEmailEdit(false);
-            setPasswordEdit(!passwordEdit);
-            resetFields();
-          }}
-          className="col-span-1 my-4 py-2 px-2 btn-text-xs"
-        >
-          Change Password
-        </button>
-        {passwordEdit && (
+          <section className="col-span-1">
+            <button
+              type="submit"
+              className="w-full py-2 btn-text-xs">
+              Submit
+            </button>
+          </section>
+        </form>
+      ):(
+        <section className="col-span-4 grid grid-cols-4 gap-x-3 grid-row-1">
           <section className="col-span-3">
-            <form onSubmit={handlePassword(onHandleSubmitPassword, onErrorPassword)}>
-              <label>
-                Password
+            <p className="heading-xs">Name</p>
+            <p className="block w-full py-1.5 px-4 mb-3 body-text-md">{user?.name}</p>
+          </section>
+          <section className="col-span-1">
+            <button
+              type="button"
+              onClick={() => {
+                setNameEdit(!nameEdit);
+                setEmailEdit(false);
+                setPasswordEdit(false);
+                resetFields();
+              }}
+              className="w-full py-2 btn-text-xs">
+              Change Name
+            </button>
+          </section>
+        </section>
+      )}
+
+      {emailEdit ? (
+        <form
+          onSubmit={handleEmail(onHandleSubmitEmail, onErrorEmail)}
+          className="col-span-4 grid grid-cols-4 gap-x-3 grid-row-1">
+          <section className="col-span-3">
+            <label className="heading-xs">
+              Email
+              <input
+                type="email"
+                {...registerEmail("email")}
+                className="block w-full py-1.5 px-4 mb-3 body-text-md focus:outline-none focus:ring focus:ring-dark-blue-50"
+              />
+              <p className="mt-1 text-center body-text-xs text-caution-200">
+                {errorsEmail.email?.message}
+              </p>
+              <p className="mt-1 text-center body-text-xs text-caution-200">
+                {formErrorEmail}
+              </p>
+            </label>
+          </section>
+          <section className="col-span-1">
+            <button
+              type="submit"
+              className="w-full py-2 btn-text-xs">
+              Submit
+            </button>
+          </section>
+        </form>
+      ):(
+        <section className="col-span-4 grid grid-cols-4 gap-x-3 grid-row-1">
+          <section className="col-span-3">
+            <p className="heading-xs">Email</p>
+            <p className="block w-full py-1.5 px-4 mb-3 body-text-md">{user?.email}</p>
+          </section>
+          <section className="col-span-1">
+            <button
+              type="button"
+              onClick={() => {
+                setNameEdit(false);
+                setEmailEdit(!emailEdit);
+                setPasswordEdit(false);
+                resetFields();
+              }}
+              className="w-full py-2 btn-text-xs">
+              Change Email
+            </button>
+          </section>
+        </section>
+      )}
+
+      {passwordEdit ? (
+        <form
+          onSubmit={handlePassword(onHandleSubmitPassword, onErrorPassword)}
+          className="col-span-4 grid grid-cols-4 gap-x-3 grid-row-1">
+          <section className="col-span-3">
+            <label className="heading-xs">
+              Password
+              <section className="relative">
                 <input
-                  type="text"
+                  type={showPassword ? "text" : "password"}
                   {...registerPassword("password")}
-                  className="body-text-md py-1.5 px-4 mb-3 w-full block focus:outline-none focus:ring focus:ring-dark-blue-50"
+                  autoComplete="new-password"
+                  className="block w-full py-1.5 px-4 mb-3 body-text-md focus:outline-none focus:ring focus:ring-dark-blue-50"
                 />
-                <p className="text-center body-text-xs text-caution-200 mt-1">
-                  {errorsPassword.password?.message}
-                </p>
-                <p className="text-center body-text-xs text-caution-200 mt-1">
-                  {formErrorPassword}
-                </p>
-              </label>
-              <button
-                type="submit"
-                className="col-span-1 my-4 py-2 btn-text-xs"
-              >
-                Submit
-              </button>
-            </form>
+                <button
+                  type="button"
+                  onClick={() => setShowPassword(!showPassword)}
+                  className="bg-grayscale-0 px-2 py-2.5 rounded-l-none absolute right-0 top-0 align-middle focus:outline-none focus:ring focus:ring-dark-blue-50">
+                  {showPassword ? <Eye size={18}/> : <EyeOff size={18}/>}
+                </button>
+              </section>
+              <p className="mt-1 text-center body-text-xs text-caution-200">
+                {errorsPassword.password?.message}
+              </p>
+              <p className="mt-1 text-center body-text-xs text-caution-200">
+                {formErrorPassword}
+              </p>
+            </label>
           </section>
-        )}
-        <section className="col-span-3 my-4">
+          <section className="col-span-1">
+            <button
+              type="submit"
+              className="w-full py-2 btn-text-xs">
+              Submit
+            </button>
+          </section>
+        </form>
+      ):(
+        <section className="col-span-4 grid grid-cols-4 gap-x-3 grid-row-1">
+          <section className="col-span-3">
+            <p className="heading-xs">Password</p>
+            <p className="w-full py-1.5 px-4 mb-3 body-text-md">Set a new password to login to your account.</p>
+          </section>
+          <section className="col-span-1">
+            <button
+              type="button"
+              onClick={() => {
+                setNameEdit(false);
+                setEmailEdit(false);
+                setPasswordEdit(!passwordEdit);
+                resetFields();
+              }}
+              className="w-full min-w-max px-[0] py-2 btn-text-xs">
+              Change Password
+            </button>
+          </section>
+        </section>
+      )}
+
+      <section className="col-span-4 grid grid-cols-4 gap-x-3 grid-row-1">
+        <section className="col-span-3">
           <p className="heading-xs">Delete Account</p>
-          <p>
-            Permanently delete the account and remove access to all projects.
+          <p className="w-full py-1.5 px-4 mb-3 body-text-md">
+              Permanently delete your account and remove access to all projects.
           </p>
         </section>
-        <button
-          type="button"
-          onClick={() => {
-            setNameEdit(false);
-            setEmailEdit(false);
-            setPasswordEdit(false);
-            setConfirmDeleteEdit(!confirmDeleteEdit);
-          }}
-          className="col-span-1 my-4 py-2 px-2 btn-text-xs bg-caution-100 hover:bg-caution-200"
-        >
-          Delete Account
-        </button>
-        {confirmDeleteEdit && (
-          <DeleteModal
-            setConfirmDeleteEdit={setConfirmDeleteEdit}
-            confirmDeleteEdit={confirmDeleteEdit}
-            handleSubmitForModal={handleSubmitForModal}
-            deleteModalText={deleteModalText} />
-        )}
-      </div>
-    </>
+        <section className="col-span-1">
+          <button
+            type="button"
+            onClick={() => {
+              setNameEdit(false);
+              setEmailEdit(false);
+              setPasswordEdit(false);
+              setConfirmDeleteEdit(!confirmDeleteEdit);
+            }}
+            className="w-full min-w-max px-[0] py-2 btn-text-xs bg-caution-100 hover:bg-caution-200">
+              Delete Account
+          </button>
+        </section>
+      </section>
+
+      {confirmDeleteEdit &&
+        <DeleteModal
+          setConfirmDeleteEdit={setConfirmDeleteEdit}
+          confirmDeleteEdit={confirmDeleteEdit}
+          handleSubmitForModal={handleSubmitForModal}
+          deleteModalText={deleteModalText} />}
+    </div>
   );
 };
