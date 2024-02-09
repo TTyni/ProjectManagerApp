5cdfc933b08e59e7c23fbb235dac4577725644a1
Harri Nieminen <moiman@posteo.net>
2024-02-09 14:56:20 +0200

    frontend: homepage cleanup (#140)

--- frontend/src/features/frontpage/HomeView.tsx
+++ frontend/src/features/frontpage/HomeView.tsx
@@ -1,19 +1,4 @@
-import { Loader } from "react-feather";
-import { useGetProjectsQuery } from "../api/apiSlice";
-
 export const HomeView = () => {
-  const { data: projects = [], isLoading, isSuccess, isError } = useGetProjectsQuery();
-
-  let projectList;
-
-  if (isLoading) {
-    projectList = <p className="mb-4"><Loader className="animate-spin" /></p>;
-  } else if (isSuccess) {
-    projectList = <ul className="list-decimal mb-4">{projects.map(p => <li key={p.id}>{p.name}</li>)}</ul>;
-  } else if (isError) {
-    projectList = <p className="mb-4">Failed to load project list</p>;
-  }
-
   return (
     <main className="grid grid-flow-col max-w-7xl m-auto mt-12">
       <div className="w-96 h-96 bg-grayscale-300 hidden mx-6 lg:flex" />
@@ -34,7 +19,6 @@ export const HomeView = () => {
           nobis dignissimos repudiandae, at maxime impedit omnis corporis
           dolores vero modi quisquam aut a explicabo.
         </p>
-        {projectList}
       </div>
     </main>
   );
