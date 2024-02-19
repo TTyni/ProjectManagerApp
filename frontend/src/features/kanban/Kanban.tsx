e4fd74f814eb06237ba7fc0aba48c39c93a78003
Harri Nieminen <moiman@posteo.net>
2024-02-19 13:58:19 +0200

    frontend: hide add column button for viewer (#178)

--- frontend/src/features/kanban/Kanban.tsx
+++ frontend/src/features/kanban/Kanban.tsx
@@ -640,6 +640,7 @@ export const Kanban = ({
                   />
                 ))}
               </SortableContext>
+              {!isUserViewer &&
               <button
                 className="w-fit min-w-fit h-[50px] px-6 py-2 mt-0.5 inline-flex items-center gap-2 rounded-sm btn-text-xs"
                 onClick={() => createNewColumn()}
@@ -647,6 +648,7 @@ export const Kanban = ({
                 <Plus size={18} />
                 Add column
               </button>
+              }
             </div>
           </div>
           {createPortal(
